f0472fd9137ddcd4cdc316bcf2c385f9788fc49f
miguelsaf <miguelarcanjo03@gmail.com>
2023-08-17 11:20:46 +0100

    Models and Dao

---
+++ template_java_project/tst/ProjectDaoTest.java
@@ -0,0 +1,20 @@
+package tst;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectDaoTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getProject() {
+    }
+
+    @Test
+    void getAllProjects() {
+    }
+}
